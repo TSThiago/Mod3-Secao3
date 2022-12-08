53e1cd12a039657f6e937044eb77b7cce53d9734
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-12-07 23:14:59 -0300

    criação da função que retorna um array com as marcas disponíveis sem repeti-las

--- 3.2/script.js
+++ 3.2/script.js
@@ -9,22 +9,23 @@ let arrayCars = []
 createCars()
 recoverValues()
 console.log(searchByBrand(arrayCars, 'fiat'))
+console.log(searchBrands(arrayCars))
 
-function createCars(e){
+function createCars(e) {
     submit.addEventListener('click', (e) => {
         let car = {
-            brand: brand.value ,
-            model: model.value ,
-            value: value.value ,
-            color: color.value ,
+            brand: brand.value,
+            model: model.value,
+            value: value.value,
+            color: color.value,
             automatic: automatic.value
         }
-        if(localStorage.length != 0){
+        if (localStorage.length != 0) {
             let newCar = car
             arrayCars = JSON.parse(window.localStorage.getItem("Cars"))
             arrayCars.push(newCar)
             window.localStorage.setItem('Cars', JSON.stringify(arrayCars))
-        }else{
+        } else {
             let newCar = car
             arrayCars.push(newCar)
             window.localStorage.setItem('Cars', JSON.stringify(arrayCars))
@@ -37,14 +38,32 @@ function createCars(e){
     })
 }
 
-function recoverValues(e){
+function recoverValues(e) {
     let jsonCar = window.localStorage.getItem("Cars")
     let car = JSON.parse(jsonCar)
     console.log(car)
 }
 
-function searchByBrand(array, brand){
+function searchByBrand(array, brand) {
     array = JSON.parse(window.localStorage.getItem("Cars"))
-    brandCars = array.filter(x => x.brand === brand)
+    let brandCars = array.filter(x => x.brand === brand)
     return brandCars
+}
+
+function searchBrands(array) {
+    let brands = []
+    array = JSON.parse(window.localStorage.getItem("Cars"))
+    brands[0] = array[0].brand
+    array.forEach(x => {
+        let counter = 0
+        brands.forEach(y => {
+            if (y === x.brand) {
+                counter++
+            }
+        });
+        if(counter === 0){
+            brands.push(x.brand)
+        }
+    })
+    return brands
 }
